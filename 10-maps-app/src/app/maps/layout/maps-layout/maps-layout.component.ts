e027205f4ea5b060fa5f42e8997eaac507431fec
Jose Vasquez <vdjoseluis@outlook.com>
2024-12-04 20:54:24 +0100

    Inicio de proyecto 10-maps-app y filesystem

---
+++ 10-maps-app/src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './maps-layout.component.html',
+  styles: ``
+})
+export class MapsLayoutComponent {
+
+}
---
+++ 10-maps-app/src/app/maps/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './properties-page.component.html',
+  styles: ``
+})
+export class PropertiesPageComponent {
+
+}
