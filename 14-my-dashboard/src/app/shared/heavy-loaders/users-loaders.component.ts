5c45fb5af45ac7db281457d9be9bcac18da18ae7
Jose Vasquez <vdjoseluis@outlook.com>
2024-12-13 11:01:46 +0100

    Create a new project and filesystem

---
+++ 14-my-dashboard/src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loaders',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Hola mundo</h1>
+  `,
+})
+export class UsersLoadersComponent {
+
+}
