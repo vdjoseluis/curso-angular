f5971b14dbf9bae745e06efcf017d007d4751a58
Jose Vasquez <vdjoseluis@outlook.com>
2025-02-11 18:57:19 +0100

    Update side-menu to allow input title and subtitle

--- 04-company-app/src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ 04-company-app/src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
+import { JvdSideMenuComponent, TitleColor } from 'jvd-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideMenuComponent],
+  imports: [CommonModule, RouterOutlet, JvdSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
   isAuthenticated = signal(false);
+
+  TitleColor = TitleColor;
 
   onLogin() {
     this.isAuthenticated.set(true);
--- 05-jvd-workspace/projects/jvd-side-menu/src/lib/jvd-side-menu.component.ts
+++ 05-jvd-workspace/projects/jvd-side-menu/src/lib/jvd-side-menu.component.ts
@@ -3,10 +3,10 @@ import { Component, input, output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export enum TitleColor {
-  red= 'text-red-500',
-  green= 'text-green-500',
-  blue= 'text-blue-500',
-  purple= 'text-purple-500'
+  red = 'text-red-500',
+  green = 'text-green-500',
+  blue = 'text-blue-500',
+  purple = 'text-purple-500'
 }
 
 @Component({
@@ -18,6 +18,9 @@ export enum TitleColor {
 })
 export class JvdSideMenuComponent {
   isAuthenticated = input(false);
+
+  title = input('JVD');
+  subtitle = input('Corp');
 
   titleColor = input<TitleColor>(TitleColor.purple);
 
