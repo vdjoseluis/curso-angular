cd498824ab87f5fb986776a5e831dc1a007f6687
Jose Vasquez <vdjoseluis@outlook.com>
2025-02-11 12:44:32 +0100

    Create a new project company-app

---
+++ 04-company-app/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./modules/admin/layouts/admin-layout/admin-layout.component'),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./modules/admin/pages/summary/summary/summary.component'),
+      },
+      {
+        path: 'projects',
+        loadComponent: () =>
+          import('./modules/admin/pages/projects/projects/projects.component'),
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
