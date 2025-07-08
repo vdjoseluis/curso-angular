54050bc2464a13de2f0c0105e259cf2bf46cce1b
Jose Vasquez <vdjoseluis@outlook.com>
2025-07-08 18:03:02 +0200

    Fin seccion 8 gifs-app

--- 2025/gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 2025/gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,32 +1,35 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifsService } from '@gifs/services/gifs.service';
-
-/* const imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-]; */
+import { ScrollStateService } from '@shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
   standalone: true,
-  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
-  styles: ``
 })
-export default class TrendingPageComponent {
-
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifsService);
-  //gifs = computed(() => this.gifService.trendingGifs());
+  scrollStateService = inject(ScrollStateService);
 
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    const { scrollTop, scrollHeight, clientHeight } = scrollDiv;
+
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
+    if (isAtBottom) {
+      this.gifService.loadTrendingGifs();
+    }
+  }
 }
--- 2025/gifs-app/src/app/gifs/services/gifs.service.ts
+++ 2025/gifs-app/src/app/gifs/services/gifs.service.ts
@@ -21,7 +21,18 @@ export class GifsService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading = signal(true);
+  trendingGifsLoading = signal(false);
+  private trendingPage = signal(0);
+
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      const group = this.trendingGifs().slice(i, i + 3);
+      groups.push(group);
+    }
+
+    return groups;
+  });
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
@@ -36,14 +47,19 @@ export class GifsService {
   });
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: environment.giphyApiKey,
         limit: 20,
+        offset: this.trendingPage() * 20,
       }
     }).subscribe((resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update((currentGifs) => [...currentGifs, ...gifs]);
+      this.trendingPage.update((page) => page + 1);
       this.trendingGifsLoading.set(false);
     });
   }
